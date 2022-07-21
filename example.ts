8fc69b1b15297606849decb03aaa8f7a5dd4c06e
navigaid <navigaid@gmail.com>
2022-07-22 06:52:36 +0800

    example: add testDenoListen / testTeleportListen

--- example.ts
+++ example.ts
@@ -1,6 +1,47 @@
 #!/usr/bin/env -S deno run -A
 
 import { connect, ConnectOptions } from "./mod.ts";
+import { Listener, ProxyListener } from "./mod.ts";
+import * as socks5 from "./socks5/mod.ts";
 
 connect({ addr: "https://example.com" });
 connect({ addr: "https://example.com/subpath/", from: "/subpath/" });
+
+const ln = new Listener({ addr: "deno.land" });
+await ln.init();
+
+// accept once
+const conn = await ln.accept();
+console.log(conn.localAddr, conn.remoteAddr);
+
+// accept loop
+for await (const conn of ln) {
+  console.log(conn.localAddr, conn.remoteAddr);
+  break;
+}
+
+// address already in use
+// const ss = socks.Server.create({ hostname: "localhost", port: 8123, });
+
+// curl -v -x socks5://127.0.0.1:8124 http://ip.sb
+async function testDenoListen() {
+  const denoListener = new ProxyListener(Deno.listen({ port: 8124 }));
+  const socks5Server = new socks5.Server(
+    { hostname: "localhost", port: 8124 },
+    () => denoListener,
+  );
+  console.log("listening on", 8124);
+  await socks5Server.listen();
+}
+
+async function testTeleportListen() {
+  const socks5Server = new socks5.Server(
+    { hostname: "localhost", port: 8123 },
+    () => ln,
+  );
+  console.log("listening on", 8123);
+  await socks5Server.listen();
+}
+
+await testDenoListen();
+// await testTeleportListen()
